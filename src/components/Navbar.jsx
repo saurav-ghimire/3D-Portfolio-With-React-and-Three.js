353f0440b1f0894b115cb0a6e9543654dc278885
saurav-ghimire <sauravghimire0123@gmail.com>
2024-03-23 12:59:15 -0400

    Nav bar Created but it is not fun working with Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {styles} from '../style'
+import { navLinks } from '../Constants'
+import {logo, menu, close} from '../assets'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex-items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+      <Link className='flex items-center gap-2' to="/" onClick={() => {
+        setActive("");
+        window.scrollTo(0,0);
+      }}>
+        <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+        <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Saurav &nbsp;
+            <span className='sm:block hidden'> | Web Development</span>
+          </p>
+      </Link>
+      <ul className='list-none hidden sm:flex flex-row gap-10'>
+        {navLinks.map((nav) => (
+              <li
+                key={nav.id}
+                className={`${
+                  active === nav.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => setActive(nav.title)}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+      </ul>
+      </div>
+    </nav>
   )
 }
 

4f3915b54f004806de41aaab3930f037cc143c6d
saurav-ghimire <sauravghimire0123@gmail.com>
2024-03-23 15:45:59 -0400

    Expirence section and Tech are Created

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -133,20 +133,7 @@ const experiences = [
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "StellR IT",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Aug 2020 - Sep 2021",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
-    title: "Senior Wordpress Developer",
+    title: "Full Stack Wordpress Developer",
     company_name: "Versity Tech Pvt. Ltd.",
     icon: tesla,
     iconBg: "#E6DEDD",
@@ -157,19 +144,6 @@ const experiences = [
       "Ensured responsiveness and cross-browser compatibility for all web projects.",
       "Collaborated closely with designers and back-end developers to create seamless user experiences.",
       "Conducted WordPress site maintenance, updates, and troubleshooting for optimal performance."
-    ],
-  },
-  {
-    title: "Full stack Developer",
-    company_name: "Meta",
-    icon: meta,
-    iconBg: "#E6DEDD",
-    date: "Jan 2023 - Present",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
     ],
   },
 ];
